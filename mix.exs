be5a2910aa9faf022bdec4b798a4aba7af110ba4
Justin Schneck <jschneck@mac.com>
2016-04-02 22:08:28 -0400

    updated license and package info for hex

--- mix.exs
+++ mix.exs
@@ -3,33 +3,33 @@ defmodule NervesSystemAlix.Mixfile do
 
   def project do
     [app: :nerves_system_alix,
-    version: "0.4.0-rc2",
-    elixir: "~> 1.2",
-    compilers: Mix.compilers ++ [:nerves_system],
-    deps: deps]
- end
+     version: "0.4.0-rc2",
+     elixir: "~> 1.2",
+     compilers: Mix.compilers ++ [:nerves_system],
+     description: description,
+     package: package,
+     deps: deps]
+  end
 
- # Configuration for the OTP application
- #
- # Type "mix help compile.app" for more information
- def application do
+  def application do
    []
- end
+  end
 
- # Dependencies can be Hex packages:
- #
- #   {:mydep, "~> 0.3.0"}
- #
- # Or git/path repositories:
- #
- #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
- #
- # Type "mix help deps" for more examples and options
- defp deps do
-   [
-     {:nerves_system, path: "../nerves_system"},
+  defp deps do
+    [{:nerves_system, path: "../nerves_system"},
      {:nerves_system_br, path: "../nerves-system-br"},
-     {:nerves_toolchain_i586_unknown_linux_gnu, github: "nerves-project/nerves_toolchain_i586_unknown_linux_gnu"}
-   ]
- end
+     {:nerves_toolchain_i586_unknown_linux_gnu, github: "nerves-project/nerves_toolchain_i586_unknown_linux_gnu"}]
+  end
+
+  defp description do
+    """
+    Nerves System - Alix http://www.pcengines.ch/alix.htm
+    """
+  end
+
+  defp package do
+    [maintainers: ["Frank Hunleth", "Justin Schneck"],
+     licenses: ["Apache 2.0"],
+     links: %{"Github" => "https://github.com/nerves-project/nerves_system_alix"}]
+  end
 end
